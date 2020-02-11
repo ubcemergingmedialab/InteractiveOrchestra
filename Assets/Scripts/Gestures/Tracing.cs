5e1a4f3491758ac6dc855939824eb1b0d223331d
jarhun88 <likisongproduct@gmail.com>
2020-02-11 14:09:12 -0800

    gesture tracking shader

---
+++ Assets/Scripts/Gestures/Tracing.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class Tracing : MonoBehaviour
+{
+
+    public GameObject batonTip;
+    bool isTracing = false;
+    Material TracingMaterial;
+    Vector3 planePoint;
+    Vector3 defaultPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TracingMaterial = GetComponent<Renderer>().material;
+        defaultPoint = new Vector3(-999, -999, -999);
+        planePoint = defaultPoint;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isTracing)
+        {
+            planePoint = batonTip.transform.position;
+            Plane clipPlane = new Plane(transform.up, planePoint);
+            Vector4 plane = new Vector4(clipPlane.normal.x, clipPlane.normal.z, clipPlane.normal.z, clipPlane.distance);
+            TracingMaterial.SetVector("_TracingPlane", plane);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Baton_Tip")
+        {
+            isTracing = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+       if(other.gameObject.name == "Baton_Tip")
+        {
+            Debug.Log("Baton Collide Exit");
+            planePoint = defaultPoint;
+            isTracing = false;
+        }
+    }
+}
